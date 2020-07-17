81fdfe0c0c36bd02c67a27bc84fc3fc9e7a110f8
Karsten Tausche <karsten@fairphone.com>
2020-07-17 19:08:51 +0200

    Avoid ninja rebuilds caused by unneeded changes of the build number file
    Looks like our custom build logic (reading the build number from VCS)
    causes the build system to regenerate build file on each rebuild. Avoid
    this by checking if the build number changed before writing it to the
    file.
    
    Issue: FP2P-421
    Change-Id: I36225e4ccdda5e5928c8acfb4eff99b53a0f8925

--- core/main.mk
+++ core/main.mk
@@ -63,9 +63,13 @@ include $(BUILD_SYSTEM)/clang/config.mk
 # Write the build number to a file so it can be read back in
 # without changing the command line every time.  Avoids rebuilds
 # when using ninja.
+# Only touch the build number file if needed, to avoid rebuilds.
+BUILD_NUMBER_FILE := $(OUT_DIR)/build_number.txt
+PREV_BUILD_NUMBER := $(file <$(BUILD_NUMBER_FILE))
+ifneq ($(BUILD_NUMBER),$(PREV_BUILD_NUMBER))
 $(shell mkdir -p $(OUT_DIR) && \
     echo -n $(BUILD_NUMBER) > $(OUT_DIR)/build_number.txt)
-BUILD_NUMBER_FILE := $(OUT_DIR)/build_number.txt
+endif
 
 ifeq ($(HOST_OS),darwin)
 DATE_FROM_FILE := date -r $(BUILD_DATETIME_FROM_FILE)
