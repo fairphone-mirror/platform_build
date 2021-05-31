19857edc3272a2de7da765514cb3ae0dfbb080c0
Gu xiaocong <xiaocong.gu@t2mobile.com>
2021-05-31 11:20:54 +0800

    udpate display_id
    Change-Id: Ia676e3771dc993513d7171f27283c49be84bb77a

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -2,10 +2,12 @@
 
 datetime=$(date +'%Y%m%d')
 
+display_version=`awk -F\" '/BUILD_AP_VER/{print $2}' version/build_version.h`
+
 if [ "$TARGET_BUILD_VARIANT" = "user" ]; then
-    BUILD_DISPLAY_ID=FP4.$BUILD_NUMBER
+    BUILD_DISPLAY_ID=FP4.$BUILD_NUMBER.$display_version-user.$datetime
 else
-    BUILD_DISPLAY_ID=FP4.$BUILD_NUMBER.$datetime-userdebug
+    BUILD_DISPLAY_ID=FP4.$BUILD_NUMBER.$display_version-userdebug.$datetime
 fi
 
 echo "# begin build properties"
