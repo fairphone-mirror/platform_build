d305fe7cd8f2bc86e2c334b2f2339d17589cb1b2
android-t1 <xiaocong.gu@t2mobile.com>
2022-08-05 09:56:58 +0800

    Add buildtime to office_version.h
    Change-Id: I148e5e616d45a62b87c96ceaafb154da002233b4

--- tools/buildinfo_common.sh
+++ tools/buildinfo_common.sh
@@ -2,7 +2,7 @@
 
 partition="$1"
 
-datetime=$(date +'%Y%m%d')
+datetime=`awk -F\- '{print $3}' version/office_version.h`
 
 display_version=`awk -F\" '/BUILD_AP_VER/{print $2}' version/build_version.h`
 
