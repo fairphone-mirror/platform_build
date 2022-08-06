0babc82ebd2189079967e283b84904c2f84903b1
android-t1 <xiaocong.gu@t2mobile.com>
2022-08-06 09:51:21 +0800

    update datime build rule
    Change-Id: Ib7f48089da293db76c0adb1dad308521c661306b

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-datetime=`awk -F\- '{print $3}' version/office_version.h`
+datetime=`awk -F\- '{print $2}' version/build_datetime.h`
 
 display_version=`awk -F\" '/BUILD_AP_VER/{print $2}' version/build_version.h`
 
