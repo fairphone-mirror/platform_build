35ce8987dbda39f1b75776ddffa15e2f1645cc19
jenkins-agent <jenkins-agent>
2025-01-09 19:39:09 +0100

    Bump build id to 6.A.031.6
    Change-Id: Iadd6e1ee7c0a863b14dd538d1948eb7c257d7a18

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,6 +18,6 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=6.A.031.5
+BUILD_ID=6.A.031.6
 
 PLATFORM_BASE_OS=Fairphone/FP3/FP3:13/6.A.030.0/gms-d57debf7:user/release-keys
